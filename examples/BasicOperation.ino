60dad9aaec35f313ee580471429b5f8f2cf6717b
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-03-01 11:14:24 +0800

    Create BasicOperation.ino

---
+++ examples/BasicOperation.ino
@@ -0,0 +1,26 @@
+/*
+  This Library is written for GSM
+  Author: Bonezegei (Jofel Batutay)
+  Date: March 2023 
+*/
+
+#include <Bonezegei_DRV8825.h>
+
+#define FORWARD 1
+#define REVERSE 0
+
+#define PIN_DIR  8
+#define PIN_STEP 9
+
+Bonezegei_DRV8825 stepper(PIN_DIR, PIN_STEP);
+
+void setup(){
+  stepper.begin();
+}
+
+void loop(){
+  stepper.step(FORWARD, 200);
+  delay(1000);
+  stepper.step(REVERSE, 200);
+  delay(1000);
+}
