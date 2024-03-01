5a6ed90c6d0d819d97b70885a6d6dd2b23ff1ade
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-03-01 22:27:36 +0800

    Update Bonezegei_DRV8825.cpp

--- src/Bonezegei_DRV8825.cpp
+++ src/Bonezegei_DRV8825.cpp
@@ -1,5 +1,5 @@
 /*
-  This Library is written for GSM
+  This Library is written for DRV8825
   Author: Bonezegei (Jofel Batutay)
   Date: March 2024 
 */
@@ -8,11 +8,13 @@
 Bonezegei_DRV8825::Bonezegei_DRV8825(int dir, int step){
   _dir=dir;
   _step=step;
+  _speed=500;
 }
 
 bool Bonezegei_DRV8825::begin(){
   pinMode(_dir, OUTPUT);
   pinMode(_step, OUTPUT);
+  return 1;
 }
 
 void Bonezegei_DRV8825::setSpeed(unsigned long speed){
@@ -25,8 +27,8 @@ void Bonezegei_DRV8825::step(int dir, int step){
   
   for(int a=0; a<step;  a++){
     digitalWrite(_step,LOW);
-    delayMicroSeconds(_speed);
+    delayMicroseconds(_speed);
     digitalWrite(_step,HIGH);
-    delayMicroSeconds(_speed);
+    delayMicroseconds(_speed);
   }
 }
