70e8a95fd0740f0ebd6be790116834871c65f6d5
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-03-01 10:53:47 +0800

    Create Bonezegei_DRV8825.h

---
+++ src/Bonezegei_DRV8825.h
@@ -0,0 +1,28 @@
+/*
+  This Library is written for GSM
+  Author: Bonezegei (Jofel Batutay)
+  Date: March 2024 
+*/
+#ifndef _BONEZEGEI_DRV8825_H_
+#define _BONEZEGEI_DRV8825_H_
+
+class Bonezegei_DRV8825{
+public:
+  //param1 = Dir Pin Direction Pin
+  //param2 = Step Pin 
+  Bonezegei_DRV8825(int dir, int step);
+  bool begin();
+
+  //speed in Micro Seconds
+  void setSpeed(unsigned long speed);
+
+  // param1 = Direction of rotation
+  // param2 = Number of Steaps
+  void step(int dir, int step);
+
+  unsigned long _speed;
+  int _dir;
+  int _step;
+};
+
+#endif
